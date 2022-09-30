1a775f4e3660db0626255111903b9bb6c5b21e03
ArifAhmed <arif15-12301@diu.edu.bd>
2022-09-30 16:46:01 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JavaScript
+ Solving Problems and Practicing
---
+++ helloworld.js
@@ -0,0 +1,6 @@
+//Ctrl + Alt + N = run
+const n = 5;
+// Looping from i = 1 to 5
+for(let i = 1; i<=n; i++){
+    console.log('Practicing Javascript');
+}
