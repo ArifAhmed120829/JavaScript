ba6f1afef76d70a86c29c5c7c9176d39e7c4621b
ArifAhmed <arif15-12301@diu.edu.bd>
2023-01-29 00:01:37 +0600

    done
    :)

---
+++ Weekday_Text.js
@@ -0,0 +1,6 @@
+
+
+var name = prompt("What is your name?");
+var num = prompt("What is your favorite number? ");
+println("Hello " + name + "!");
+println(num + "?! That's my favorite number too!");
---
+++ script.js
@@ -0,0 +1,11 @@
+// Start Writing Code Here
+<li onclick = "showAlert()">About this document</li>
+
+function showAlert(){
+    alert('Alert by Function Call');
+}
+
+
+    
+
+
