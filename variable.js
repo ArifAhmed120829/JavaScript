3536069f609ec182a183b28cfaf0f009d0ed47ae
ArifAhmed <arif15-12301@diu.edu.bd>
2023-04-27 20:50:07 +0600

    ez
    ez

---
+++ variable.js
@@ -0,0 +1,27 @@
+var $text = 'I am valid!';
+console.log('My name is ' +$text);
+//funcname();
+function funcname(){
+    console.log('hello world');
+}
+var array = ['hello','nigga','bokacoda'];
+array.push('arif');
+console.log(array[2]);
+var times = 5;
+while(times!= 0){
+    console.log('wait..');
+    times--;
+}
+console.log('start');
+
+var cat = {
+    name: 'Chloe',
+    age: 5,
+    color: 'gray',
+    meow: function () {
+      console.log('meow meow');
+    }
+  };
+  
+
+  console.log(cat.meow);
